e6cdccb9fed8855754fea5d37cd40f8f1397d69b
NickGaming53 <nickgaming8889@gmail.com>
2023-02-20 00:15:14 -0600

    Versión 1.10.3

--- Menu.c
+++ Menu.c
@@ -3,5 +3,39 @@
 
 void main() {
     
+    int opc, nsm, placa, year;
+    char nv, mdl, name;
+
+    do
+    {
+        system("cls");
+        //Opciones de menu...
+        printf("Bienvenido al Menu...\n");
+        printf("\n1. Registro del Cliente y/o Vehiculo.");
+        printf("\n2. Inspección y Estimación de Costo.");
+        printf("\n3. Almacen.");
+        printf("\n4. Progreso de Reparación.");
+        //Pide la opción a elegir...
+        printf("\nQue deseas hacer hoy?..");
+        gets(opc);
+
+        //Sección del switch (aqui ira cada caso)...
+        switch (opc) {
+        case 1:
+            //Solicitar datos de cliente y el vehiculo...
+            printf("Registro del Cliente y/o Vehiculo:");
+
+            //Datos del Cliente...
+            printf("Nombre del Cliente: ");
+            gets(name);
+            
+            break;
+        
+        default:
+            break;
+        }
+    } while ();
+    
+
     return 0;
 }
