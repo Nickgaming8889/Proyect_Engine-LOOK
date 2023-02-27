676affaab8cff2a7f27276a0f6aa05787af07476
Nick <nickgaming8889@gmail.com>
2023-02-26 18:50:49 -0600

    Versión 1.10.3

--- Menu.c
+++ Menu.c
@@ -8,22 +8,52 @@ int main() {
     do
     {
         //Secciones del menu principal...
+        main_menu:
+        system("cls");
         printf("\nBienvenido el Menu...");
         printf("\n\n1. Administracion.");
         printf("\n2. Almacen y Compras.");
         printf("\n3. Salir del Programa.");
 
-        //Solicitar dato de entrada...
+        //Solicitar dato de entradaa submenu...
         printf("\n\nSeleccione el numero del submenu.. ");
         scanf("%i", &op);
 
+        //Redireccion al submenu...
         switch (op) {
         case 1:
+            //Admi:
+            system("cls");
             printf("\nEstas en Administracion.");
                 do
                 {
+                    printf("\n1. Registro del cliente.");
+                    printf("\n2. Baja de cliente.");
+                    printf("\n3. Cotizacion de reparacion.");
+                    printf("\n4. Volver al menu principal.");
                     
-                } while ();
+                    //Solicitar dato de entrada al area requerida...
+                    printf("\n\nSeleccione la opcion que necesite: ");
+                    scanf("%i", &op);
+
+                    switch (op) {
+                    case 1:
+                        printf("\nEstas en Registro de Cliente...\n");
+                        break;
+                    case 2:
+                        printf("\nEstas en Baja de Cliente...\n");
+                        break;
+                    case 3:
+                        printf("\nEstas en Cotizacion de Reparacion...\n");
+                        break;
+                    case 4:
+                        goto main_menu;
+                        break;
+                    default:
+                        break;
+                    }
+
+                } while (op != 4);
                 
             break;
         case 2:
@@ -33,6 +63,7 @@ int main() {
             printf("\nHaz salido del programa.");
             break;
         default:
+            printf("\nEl numero no valido, intente nuevamente");
             break;
         }
     } while (op != 3);
