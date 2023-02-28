51d2a958627ac8b2d0b2a73f9499e6a325cc8661
NickGaming53 <nickgaming8889@gmail.com>
2023-02-28 00:21:39 -0600

    Versión 1.10.3

--- Menu.c
+++ Menu.c
@@ -22,15 +22,15 @@ int main() {
         //Redireccion al submenu...
         switch (op) {
         case 1:
-            //Admi:
             system("cls");
-            printf("\nEstas en Administracion.");
+            printf("\nEstas en Administracion.\n");
                 do
                 {
                     printf("\n1. Registro del cliente.");
                     printf("\n2. Baja de cliente.");
                     printf("\n3. Cotizacion de reparacion.");
-                    printf("\n4. Volver al menu principal.");
+                    printf("\n4. Proceso de Trabajo.");
+                    printf("\n5. Volver al menu principal.");
                     
                     //Solicitar dato de entrada al area requerida...
                     printf("\n\nSeleccione la opcion que necesite: ");
@@ -38,26 +38,64 @@ int main() {
 
                     switch (op) {
                     case 1:
+                        system("cls");
                         printf("\nEstas en Registro de Cliente...\n");
                         break;
                     case 2:
+                        system("cls");
                         printf("\nEstas en Baja de Cliente...\n");
                         break;
                     case 3:
+                        system("cls");
                         printf("\nEstas en Cotizacion de Reparacion...\n");
                         break;
                     case 4:
+                        system("cls");
+                        printf("\nEstas en Proceso de Trabajo...\n");
+                        break;
+                    case 5:
+                        system("cls");
                         goto main_menu;
                         break;
                     default:
+                        printf("\nNumero no valido, intente nuevamente...\n");
                         break;
                     }
 
-                } while (op != 4);
+                } while (op != 5);
                 
             break;
         case 2:
-            printf("\nEstas en Almacen y Compras.");
+            system("cls");
+            printf("\nEstas en Almacen y Compras.\n");
+                do
+                {
+                    printf("\n1. Almacen.");
+                    printf("\n2. Comprar piezas.");
+                    printf("\n3. Volver al menu principal.");
+
+                    printf("\n\nSelecione la opcion que necesite: ");
+                    scanf("%i", &op);
+
+                    switch (op) {
+                    case 1:
+                        system("cls");
+                        printf("\nEstas en el Almacen...\n");
+                        break;
+                    case 2:
+                        system("cls");
+                        printf("\nEstas en Compra de Piezas...\n");
+                        break;
+                    case 3:
+                        system("cls");
+                        goto main_menu;
+                        break;
+                    default:
+                        printf("\nNumero no valido, intente nuevamente...\n");
+                        break;
+                    }
+                } while (op != 3);
+
             break;
         case 3:
             printf("\nHaz salido del programa.");
