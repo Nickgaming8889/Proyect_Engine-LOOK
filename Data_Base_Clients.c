b66d8aaabbf874ab056a648eac9fc1cb12a33442
NickGaming53 <nickgaming8889@gmail.com>
2023-03-20 00:08:50 -0600

    Versión 1.10.4

--- Data_Base_Clients.c
+++ Data_Base_Clients.c
@@ -14,12 +14,39 @@ struct clients n_o[100];
 
 int main() {
 
-    int i, back = 0;
+    int i, j, back = 0, end = 0;
 
     do
     {
-        /* code */
-    } while ();
+        system("cls");
+        fflush(stdin);
+        for (i = back; i <= 100&&end != 1; i++)
+        {
+            puts("Client Name: ");
+            gets(n_o[i].name_clt);
+            puts("Client Phone: ");
+            gets(n_o[i].tel);
+            puts("Vehicle Name: ");
+            gets(n_o[i].mk);
+            puts("Engine Serie: ");
+            gets(n_o[i].nsm);
+            puts("Year: ");
+            gets(n_o[i].year);
+            puts("license Plate: ");
+            gets(n_o[i].plt);
+
+            printf("Do you want to continue adding? y/n");
+            scanf("%d",&j);
+
+            if (j = 'y'){
+                /* code */
+            }
+            
+
+        }
+        
+        
+    } while (i != 101);
     
 
     return 0;
