40895855bda651919ea8b5d7ba1c83d68fce5149
Nickgaming8889 <92138380+Nickgaming8889@users.noreply.github.com>
2023-05-10 01:08:58 -0600

    Versión 1.10.8

--- RorN.c
+++ RorN.c
@@ -62,33 +62,33 @@ int main(int argc, char*argv[]) {
         case 4: {
 
             printf("\nPiston 1 Measurements Transverse and Long: ");
-            scanf("%f %f", &engine->var.piston_1T, &engine[0].var.piston_1L);
+            scanf("%f %f", &engine.var.piston_1T, &engine.var.piston_1L);
 
             printf("\nPiston 2 Measurements Transverse and Long: ");
-            scanf("%f %f", &engine->var.piston_2T, &engine->var.piston_2L);
+            scanf("%f %f", &engine.var.piston_2T, &engine.var.piston_2L);
 
             printf("\nPiston 3 Measurements Transverse and Long: ");
-            scanf("%f %f", &engine->var.piston_3T, &engine->var.piston_3L);
+            scanf("%f %f", &engine.var.piston_3T, &engine.var.piston_3L);
 
             printf("\nPiston 4 Measurements Transverse and Long: ");
-            scanf("%f %f", &engine->var.piston_4T, &engine->var.piston_4L);
+            scanf("%f %f", &engine.var.piston_4T, &engine.var.piston_4L);
 
-            float max1 = fmax(engine->var.piston_1T, fmax(engine->var.piston_2T, fmax(engine->var.piston_3T, engine->var.piston_4T)));
-            float max2 = fmax(engine->var.piston_1L, fmax(engine->var.piston_2L, fmax(engine->var.piston_3L, engine->var.piston_4L)));
+            float max1 = fmax(engine.var.piston_1T, fmax(engine.var.piston_2T, fmax(engine.var.piston_3T, engine.var.piston_4T)));
+            float max2 = fmax(engine.var.piston_1L, fmax(engine.var.piston_2L, fmax(engine.var.piston_3L, engine.var.piston_4L)));
             float mmax = fmax(max1, max2);
 
             printf("%f", mmax);
 
-            if (mmax > engine->var.stdpiston_nissan && mmax < engine->var.limitstd_nissan) {
+            if (mmax >= engine.var.stdpiston_nissan || mmax <= engine.var.limitstd_nissan) {
                 printf("By the moment it don need to be rectify");
             }
-            else if (mmax > engine->var.limitstd_nissan && mmax < engine->var.toten) {
+            else if (mmax >= engine.var.limitstd_nissan || mmax <= engine.var.toten) {
                 printf("Rectification to 0.10mm.");
             }
-            else if (mmax > engine->var.toten && mmax < engine->var.totwenty) {
+            else if (mmax > engine.var.toten || mmax < engine.var.totwenty) {
                 printf("Rectification to 0.20mm.");
             }
-            else if (mmax > engine->var.totwenty && mmax < engine->var.tothirty) {
+            else if (mmax > engine.var.totwenty || mmax < engine.var.tothirty) {
                 printf("Rectification to 0.30mm.");
             }
             else {
@@ -99,39 +99,39 @@ int main(int argc, char*argv[]) {
         }
         case 6: {
             printf("\nPiston 1 Measurements Transverse and Long: ");
-            scanf("%f %f", &engine->var.piston_1T, &engine->var.piston_1L);
+            scanf("%f %f", &engine.var.piston_1T, &engine.var.piston_1L);
 
             printf("\nPiston 2 Measurements Transverse and Long: ");
-            scanf("%f %f", &engine->var.piston_2T, &engine->var.piston_2L);
+            scanf("%f %f", &engine.var.piston_2T, &engine.var.piston_2L);
 
             printf("\nPiston 3 Measurements Transverse and Long: ");
-            scanf("%f %f", &engine->var.piston_3T, &engine->var.piston_3L);
+            scanf("%f %f", &engine.var.piston_3T, &engine.var.piston_3L);
 
             printf("\nPiston 4 Measurements Transverse and Long: ");
-            scanf("%f %f", &engine->var.piston_4T, &engine->var.piston_4L);
+            scanf("%f %f", &engine.var.piston_4T, &engine.var.piston_4L);
 
             printf("\nPiston 5 Measurements Transverse and Long: ");
-            scanf("%f %f", &engine->var.piston_5T, &engine->var.piston_5L);
+            scanf("%f %f", &engine.var.piston_5T, &engine.var.piston_5L);
 
             printf("\nPiston 6 Measurements Transverse and Long: ");
-            scanf("%f %f", &engine->var.piston_6T, &engine->var.piston_6L);
+            scanf("%f %f", &engine.var.piston_6T, &engine.var.piston_6L);
 
-            float max1 = fmax(engine->var.piston_1T, fmax(engine->var.piston_2T, fmax(engine->var.piston_3T, fmax(engine->var.piston_4T, fmax(engine->var.piston_5T, engine->var.piston_6T)))));
-            float max2 = fmax(engine->var.piston_1L, fmax(engine->var.piston_2L, fmax(engine->var.piston_3L, fmax(engine->var.piston_4L, fmax(engine->var.piston_5L, engine->var.piston_6L)))));
+            float max1 = fmax(engine.var.piston_1T, fmax(engine.var.piston_2T, fmax(engine.var.piston_3T, fmax(engine.var.piston_4T, fmax(engine.var.piston_5T, engine.var.piston_6T)))));
+            float max2 = fmax(engine.var.piston_1L, fmax(engine.var.piston_2L, fmax(engine.var.piston_3L, fmax(engine.var.piston_4L, fmax(engine.var.piston_5L, engine.var.piston_6L)))));
             float mmax = fmax(max1, max2);
 
             printf("%f", mmax);
 
-            if (mmax >= engine->var.stdpiston_nissan && mmax <= engine->var.limitstd_nissan) {
+            if (mmax > engine.var.stdpiston_nissan && mmax < engine.var.limitstd_nissan) {
                 printf("By the moment it don need to be rectify");
             }
-            else if (mmax > engine->var.limitstd_nissan && mmax < engine->var.toten) {
+            else if (mmax > engine.var.limitstd_nissan && mmax < engine.var.toten) {
                 printf("Rectification to 0.10mm.");
             }
-            else if (mmax > engine->var.toten && mmax < engine->var.totwenty) {
+            else if (mmax > engine.var.toten && mmax < engine.var.totwenty) {
                 printf("Rectification to 0.20mm.");
             }
-            else if (mmax > engine->var.totwenty && mmax < engine->var.tothirty) {
+            else if (mmax > engine.var.totwenty && mmax < engine.var.tothirty) {
                 printf("Rectification to 0.30mm.");
             }
             else {
@@ -141,45 +141,45 @@ int main(int argc, char*argv[]) {
         }
         case 8: {
             printf("\nPiston 1 Measurements Transverse and Long: ");
-            scanf("%f %f", &engine->var.piston_1T, &engine->var.piston_1L);
+            scanf("%f %f", &engine.var.piston_1T, &engine.var.piston_1L);
 
             printf("\nPiston 2 Measurements Transverse and Long: ");
-            scanf("%f %f", &engine->var.piston_2T, &engine->var.piston_2L);
+            scanf("%f %f", &engine.var.piston_2T, &engine.var.piston_2L);
 
             printf("\nPiston 3 Measurements Transverse and Long: ");
-            scanf("%f %f", &engine->var.piston_3T, &engine->var.piston_3L);
+            scanf("%f %f", &engine.var.piston_3T, &engine.var.piston_3L);
 
             printf("\nPiston 4 Measurements Transverse and Long: ");
-            scanf("%f %f", &engine->var.piston_4T, &engine->var.piston_4L);
+            scanf("%f %f", &engine.var.piston_4T, &engine.var.piston_4L);
 
             printf("\nPiston 5 Measurements Transverse and Long: ");
-            scanf("%f %f", &engine->var.piston_5T, &engine->var.piston_5L);
+            scanf("%f %f", &engine.var.piston_5T, &engine.var.piston_5L);
 
             printf("\nPiston 6 Measurements Transverse and Long: ");
-            scanf("%f %f", &engine->var.piston_6T, &engine->var.piston_6L);
+            scanf("%f %f", &engine.var.piston_6T, &engine.var.piston_6L);
 
             printf("\nPiston 6 Measurements Transverse and Long: ");
-            scanf("%f %f", &engine->var.piston_7T, &engine->var.piston_7L);
+            scanf("%f %f", &engine.var.piston_7T, &engine.var.piston_7L);
 
             printf("\nPiston 6 Measurements Transverse and Long: ");
-            scanf("%f %f", &engine->var.piston_8T, &engine->var.piston_8L);
+            scanf("%f %f", &engine.var.piston_8T, &engine.var.piston_8L);
 
-            float max1 = fmax(engine->var.piston_1T, fmax(engine->var.piston_2T, fmax(engine->var.piston_3T, fmax(engine->var.piston_4T, fmax(engine->var.piston_5T, fmax(engine->var.piston_6T, fmax(engine->var.piston_7T, engine->var.piston_8T)))))));
-            float max2 = fmax(engine->var.piston_1L, fmax(engine->var.piston_2L, fmax(engine->var.piston_3L, fmax(engine->var.piston_4L, fmax(engine->var.piston_5L, fmax(engine->var.piston_6L, fmax(engine->var.piston_7L, engine->var.piston_8L)))))));
+            float max1 = fmax(engine.var.piston_1T, fmax(engine.var.piston_2T, fmax(engine.var.piston_3T, fmax(engine.var.piston_4T, fmax(engine.var.piston_5T, fmax(engine.var.piston_6T, fmax(engine.var.piston_7T, engine.var.piston_8T)))))));
+            float max2 = fmax(engine.var.piston_1L, fmax(engine.var.piston_2L, fmax(engine.var.piston_3L, fmax(engine.var.piston_4L, fmax(engine.var.piston_5L, fmax(engine.var.piston_6L, fmax(engine.var.piston_7L, engine.var.piston_8L)))))));
             float mmax = fmax(max1, max2);
 
             printf("%f", mmax);
 
-            if (mmax >= engine->var.stdpiston_nissan && mmax <= engine->var.limitstd_nissan) {
+            if (mmax > engine.var.stdpiston_nissan && mmax < engine.var.limitstd_nissan) {
                 printf("By the moment it don need to be rectify");
             }
-            else if (mmax > engine->var.limitstd_nissan && mmax < engine->var.toten) {
+            else if (mmax > engine.var.limitstd_nissan && mmax < engine.var.toten) {
                 printf("Rectification to 0.10mm.");
             }
-            else if (mmax > engine->var.toten && mmax < engine->var.totwenty) {
+            else if (mmax > engine.var.toten && mmax < engine.var.totwenty) {
                 printf("Rectification to 0.20mm.");
             }
-            else if (mmax > engine->var.totwenty && mmax < engine->var.tothirty) {
+            else if (mmax > engine.var.totwenty && mmax < engine.var.tothirty) {
                 printf("Rectification to 0.30mm.");
             }
             else {
